16126aa35aa83b32acf1e14ce971e54fb17a6f53
MHPontes <mateushspontes@gmail.com>
2024-06-17 14:23:32 -0300

    Entendendo props e component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const Bemvindo = (props) => {
   return (
     <div>
       <h2>Bem-vindo(a) {props.nome} </h2>
+      <h3>Tenho {props.idade} Anos</h3>
     </div>
   );
 }
@@ -14,7 +15,8 @@ function App() {
   return (
     <div>
       Olá, mundo!
-      <Bemvindo nome="Joana"/>
+      <Bemvindo nome="Mateus" idade="29"/>
+      <Bemvindo nome="Joana" idade="19"/>
       <h1>Curso React</h1>
     </div>
   );
