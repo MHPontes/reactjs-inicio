8966d18369a86c8955880b62e071f6f526d28a13
MHPontes <mateushspontes@gmail.com>
2024-06-17 14:18:19 -0300

    Iniciando em ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// const Bemvindo = () => <h2>Bem-vindo(a)</h2>
+
+const Bemvindo = (props) => {
+  return (
+    <div>
+      <h2>Bem-vindo(a) {props.nome} </h2>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Olá, mundo!
+      <Bemvindo nome="Joana"/>
+      <h1>Curso React</h1>
     </div>
   );
 }
