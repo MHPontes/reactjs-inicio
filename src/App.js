30577dbfd380be4e8e499953c34e7f8507df4096
MHPontes <mateushspontes@gmail.com>
2024-06-18 09:56:16 -0300

    Aprendendo sobre Eventos e separando classes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hora: '00:00:00'
-    };
-  }
-
-  componentDidMount() {
-
-    setInterval(() => {
-      this.setState({ hora: new Date().toLocaleTimeString() });
-    }, 1000);
-
-  }
-
-  componentDidUpdate() {
-    console.log('Atualizou');
-  }
-
   render() {
-    return (
-      <div>
-        <h1>Meu projeto {this.state.hora}</h1>
-      </div>
-    );
+      return (
+          <div>
+            <Membro nome="Visitante"/>
+          </div>
+      );
   }
+
 }
 
 export default App;
+
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       hora: '00:00:00'
+//     };
+//   }
+
+//   componentDidMount() {
+
+//     setInterval(() => {
+//       this.setState({ hora: new Date().toLocaleTimeString() });
+//     }, 1000);
+
+//   }
+
+//   componentDidUpdate() {
+//     console.log('Atualizou');
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Meu projeto {this.state.hora}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 // class App extends Component {
   
