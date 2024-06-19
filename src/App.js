3c39ba8bb6031104fd8fa9966f34b2236e9ac15b
MHPontes <mateushspontes@gmail.com>
2024-06-19 10:11:27 -0300

    Manipulando formularios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
-import Feed from './components/Feed';
+// import Feed from './components/Feed';
 // import Membro from './components/Membro';
 
+
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+      sexo: 'masculino'
+    };
+
+    this.trocaEmail = this.trocaEmail.bind(this); //Faz a ligação do método com o construtor
+  }
+
+  trocaEmail(e) {
+    let valorDigitado = e.target.value;
+    this.setState({ email: valorDigitado });
+  }
+
+
   render() {
     return (
       <div>
-        <Feed/>
+        <h2>Login</h2>
+        Email:
+        <input type="email" name="email" value={this.state.email}
+          onChange={this.trocaEmail} />   {/* //Chama o método trocaEmail */}
+        <br />
+        Senha:
+        <input type="password" name="senha" value={this.state.senha}
+          onChange={(e) => this.setState({ senha: e.target.value })} />  
+        <br />
+        <br />
+        Sexo:
+        <select name="sexo" value={this.state.sexo}
+          onChange={(e) => this.setState({ sexo: e.target.value })}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+        </select>
+
+        <div> 
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+          <h3>{this.state.sexo}</h3>
+          </div>
+
       </div>
     );
-}
+  }
 }
 
 export default App;
+
+// class App extends Component {
+  
+//   render() {
+//     return (
+//       <div>
+//         <Feed/>
+//       </div>
+//     );
+// }
+// }
+
+// export default App;
 
 // class App extends Component {
 
